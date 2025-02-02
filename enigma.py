1348f340b1247fca4a48a75ca1809bbc58647d2c
ShayMidginski <shay.magik@gmail.com>
2025-02-02 21:55:59 +0200

    finished all functions. ready for tests

--- enigma.py
+++ enigma.py
@@ -5,7 +5,7 @@ import sys
 get_params_exit_messege = "Usage: python3 enigma.py -c <config_file> -i <input_file> -o <output_file>"
 script_exit_messege = "The enigma script has encountered an error"
 
-# ============ ENIGMA ============
+# ==================== ENIGMA ==================== #
 
 class Enigma:
 
@@ -21,11 +21,11 @@ class Enigma:
         encrypted_message = ""
         for letter in message:
             if letter.islower():
-                encrypted_message += encrypt_lowercase_letter(self, letter)
+                encrypted_message += self.encrypt_lowercase_letter(letter)
                 encrypted_count += 1
             else:
                 encrypted_message += letter
-            update_wheels(encrypted_count)
+            enigma.update_wheels(encrypted_count)
         self.wheels = original_wheels.copy()
         return encrypted_message
 
@@ -48,15 +48,36 @@ class Enigma:
             self.wheels[2] = 5
         else:
             self.wheels[2] = 0
+            
+            
+    def encrypt_lowercase_letter(self, letter):
+        reversed_hash_map = {value: key for key, value in self.hash_map.items()}
+        i = self.hash_map[letter]
+        wheels_value = ((2 * self.wheels[0]) - self.wheels[1] + self.wheels[2]) % 26
+        if wheels_value != 0:
+            i += wheels_value
+        else:
+            i += 1
+        i %= 26
+        c1 = reversed_hash_map[i]
+        c2 = self.reflector_map[c1]
+        i = self.hash_map[c2]
+        if wheels_value == 0:
+            i -= 1
+        else:
+            i -= wheels_value
+        i %= 26
+        c3 = reversed_hash_map[i]
+        return c3
 
 
-# ============ JSONFileException ============
+# ==================== JSONFileException ==================== #
 
 class JSONFileException(Exception):
     pass
 
 
-# ============ OTHER FUNCTIONS ============
+# ==================== OTHER FUNCTIONS ==================== #
 
 def load_enigma_from_path(path):
     try:
@@ -67,29 +88,11 @@ def load_enigma_from_path(path):
         raise JSONFileException
 
 
-def encrypt_lowercase_letter(enigma, letter):
-    i = enigma.hash_map(letter)
-    wheels_value = ((2 * enigma.wheels[0]) - enigma.wheels[1] + enigma.wheels[2]) % 26
-    if wheels_value != 0:
-        i += wheels_value
-    else:
-        i += 1
-
-    i %= 26
-    c1 = enigma.hash_map(i)
-    c2 = enigma.reflector_map[c1]
-
-    i = enigma.hash_map(c2)
-    if wheels_value == 0:
-        i -= 1
-    else:
-        i -= wheels_value
-    i %= 26
-    c3 = enigma.hash_map(i)
-    return c3
-
 def get_params():
     params = sys.argv
+    config_path = None
+    input_file = None
+    output_path = sys.stdout
     for i in range(1, len(params), 2):
         if i + 1 >= len(params):
             terminate(get_params_exit_messege)
@@ -98,13 +101,11 @@ def get_params():
         elif params[i] == '-i':
             input_path = params[i + 1]
         elif params[i] == '-o':
-            output_path == params[i + 1]
+            output_path = params[i + 1]
         else:
             terminate(get_params_exit_messege)
     if not config_path or not input_path:
         terminate(get_params_exit_messege)
-    if not output_path:
-        output_path = sys.stdout
     return config_path, input_path, output_path
     
     
@@ -113,20 +114,21 @@ def terminate(messege):
     exit(1)
 
 
+# ==================== SCRIPT ==================== #
+
 if __name__ == "__main__":
         config_file, input_path, output_path = get_params()
-        enigma = load_enigma_from_path(config_file)
         try:
+            enigma = load_enigma_from_path(config_file)
             with open(input_path, 'r') as input:
-                encrypted_str = enigma.encrypt(input)
+                encrypted_str = enigma.encrypt(input.read())
         except Exception:
             terminate(script_exit_messege)
-        if not output_path == sys.stdout:
+        if output_path == sys.stdout:
+            print(encrypted_str)
+        else:
             try:
                 with open(output_path, 'w') as output:
                     output.write(encrypted_str)
             except Exception:
                 terminate(script_exit_messege)
-        else:
-            print(encrypted_str)
-C
