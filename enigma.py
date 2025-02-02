167879a7edd4ea0710804b6a3917b3fecd14cf1a
ShayMidginski <shay.magik@gmail.com>
2025-02-02 22:10:43 +0200

    define consts as global vars

--- enigma.py
+++ enigma.py
@@ -2,8 +2,17 @@
 import json
 import sys
 
-get_params_exit_messege = "Usage: python3 enigma.py -c <config_file> -i <input_file> -o <output_file>"
-script_exit_messege = "The enigma script has encountered an error"
+W1 = 0
+W2 = 1
+W3 = 2
+W1_UPDATE_FLAG = 8
+W3_UPDATE_FLAG_1 = 10
+W3_UPDATE_FLAG_2 = 3
+W3_UPDATE_VALUE_1 = 10
+W3_UPDATE_VALUE_2 = 5
+WHEELS_VALUE_MOD = 26
+GET_PARAMS_EXIT_MESSEGE = "Usage: python3 enigma.py -c <config_file> -i <input_file> -o <output_file>"
+SCRIPT_EXIT_MESSEGE = "The enigma script has encountered an error"
 
 # ==================== ENIGMA ==================== #
 
@@ -32,41 +41,41 @@ class Enigma:
 
     def update_wheels(self,encrypted_count):
         # update W1
-        if self.wheels[0] + 1 > 8:
-            self.wheels[0] = 1
+        if self.wheels[W1] + 1 > W1_UPDATE_FLAG:
+            self.wheels[W1] = 1
         else:
-            self.wheels[0] += 1
+            self.wheels[W1] += 1
         # update W2
         if encrypted_count % 2 == 0:
-            self.wheels[1] *= 2
+            self.wheels[W2] *= 2
         else:
-            self.wheels[1] -= 1
+            self.wheels[W2] -= 1
         # update W3
-        if encrypted_count % 10 == 0:
-            self.wheels[2] = 10
-        elif encrypted_count % 3 == 0:
-            self.wheels[2] = 5
+        if encrypted_count % W3_UPDATE_FLAG_1 == 0:
+            self.wheels[W3] = W3_UPDATE_VALUE_1
+        elif encrypted_count % W3_UPDATE_FLAG_2 == 0:
+            self.wheels[W3] = W3_UPDATE_VALUE_2
         else:
-            self.wheels[2] = 0
+            self.wheels[W3] = 0
             
             
-    def encrypt_lowercase_letter(self, letter):
+    def encrypt_lowercase_letter(self, c):
         reversed_hash_map = {value: key for key, value in self.hash_map.items()}
-        i = self.hash_map[letter]
-        wheels_value = ((2 * self.wheels[0]) - self.wheels[1] + self.wheels[2]) % 26
+        i = self.hash_map[c]
+        wheels_value = ((2 * self.wheels[W1]) - self.wheels[W2] + self.wheels[W3]) % WHEELS_VALUE_MOD
         if wheels_value != 0:
             i += wheels_value
         else:
             i += 1
-        i %= 26
+        i %= WHEELS_VALUE_MOD
         c1 = reversed_hash_map[i]
         c2 = self.reflector_map[c1]
         i = self.hash_map[c2]
-        if wheels_value == 0:
-            i -= 1
-        else:
+        if wheels_value != 0:
             i -= wheels_value
-        i %= 26
+        else:
+            i -= 1
+        i %= WHEELS_VALUE_MOD
         c3 = reversed_hash_map[i]
         return c3
 
@@ -95,7 +104,7 @@ def get_params():
     output_path = sys.stdout
     for i in range(1, len(params), 2):
         if i + 1 >= len(params):
-            terminate(get_params_exit_messege)
+            terminate(GET_PARAMS_EXIT_MESSEGE)
         if params[i] == '-c':
             config_path = params[i + 1]
         elif params[i] == '-i':
@@ -103,9 +112,9 @@ def get_params():
         elif params[i] == '-o':
             output_path = params[i + 1]
         else:
-            terminate(get_params_exit_messege)
+            terminate(GET_PARAMS_EXIT_MESSEGE)
     if not config_path or not input_path:
-        terminate(get_params_exit_messege)
+        terminate(GET_PARAMS_EXIT_MESSEGE)
     return config_path, input_path, output_path
     
     
@@ -123,7 +132,7 @@ if __name__ == "__main__":
             with open(input_path, 'r') as input:
                 encrypted_str = enigma.encrypt(input.read())
         except Exception:
-            terminate(script_exit_messege)
+            terminate(SCRIPT_EXIT_MESSEGE)
         if output_path == sys.stdout:
             print(encrypted_str)
         else:
@@ -131,4 +140,4 @@ if __name__ == "__main__":
                 with open(output_path, 'w') as output:
                     output.write(encrypted_str)
             except Exception:
-                terminate(script_exit_messege)
+                terminate(SCRIPT_EXIT_MESSEGE)
